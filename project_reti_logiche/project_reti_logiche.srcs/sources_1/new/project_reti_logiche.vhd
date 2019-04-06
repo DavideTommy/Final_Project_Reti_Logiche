9522ac6f8b6095ba69007ede61229f1574a0f318
Federico Lichinchi <10531741@polimi.it>
2019-04-06 10:48:18 +0200

    Prova di next con prevState.

--- project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche/project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: 
+-- Engineer: Lichinchi & Lorenzi
 -- 
--- Create Date: 20.03.2019 19:18:41
+-- Create Date: 28.03.2019 15:32:58
 -- Design Name: 
--- Module Name: project_reti_logiche - Behavioral
--- Project Name: 
+-- Module Name: progetto_reti_logiche - Behavioral
+-- Project Name: Final Project Reti Logiche
 -- Target Devices: 
 -- Tool Versions: 
 -- Description: 
@@ -33,7 +33,7 @@ use IEEE.numeric_std.ALL;
 --use UNISIM.VComponents.all;
 
 entity project_reti_logiche is
-    port (
+  port (
 -- i_clk è il segnale di CLOCK in ingresso generato dal TestBench    
         i_clk : in std_logic;
 -- i_start è il segnale di START generato dal Test Bench;        
@@ -42,7 +42,7 @@ entity project_reti_logiche is
         i_rst : in std_logic;
 -- i_data è il segnale (vettore) che arriva dalla memoria in seguito ad una richiesta di lettura        
         i_data : in std_logic_vector(7 downto 0);
--- o_address è il segnale (vettore) di uscita che manda l’indirizzo alla memoria        
+-- o_address è il segnale (vettore) di uscita che manda l'indirizzo alla memoria        
         o_address : out std_logic_vector(15 downto 0);
 -- o_done è il segnale di uscita che comunica la fine dell'elaborazione e il dato di uscita scritto in memoria        
         o_done : out std_logic;
@@ -57,159 +57,214 @@ entity project_reti_logiche is
 end project_reti_logiche;
 
 architecture Behavioral of project_reti_logiche is
-    type STATO is ( RST, S0, S1, S2, S3, S4, S5, S6, S7, S8 );
-    type D_ARRAY is array (8191 downto 0) of unsigned(9 downto 0);
-    signal PS, NS, PRS, PRS_prev : STATO;
-    signal Y_out : std_logic_vector (7 downto 0);
-    signal Yp, Xp, Xp_prev : unsigned (9 downto 0);
-    signal Yo, Xo, Yo_prev, Xo_prev : unsigned (9 downto 0);
-    signal Ydiff, Xdiff : unsigned (9 downto 0);
-    signal bitMask, bitMask_prev : std_logic_vector(7 downto 0) := (others => '0');
-    signal distances : D_ARRAY := (others => to_unsigned(520,10));
-    signal min_distance,min_distance_prev : unsigned := to_unsigned(520,10);
-    signal dist_tmp : unsigned (9 downto 0);
-    signal counter,counter_prev : integer := 1;
-    signal counter2,counter2_prev : integer := 7;
+    type STATO is (RST,BEGINNER,RAM_WAIT,READ,SAVE,BEGIN_COMPUTE,COMPUTE,COMPARE,WRITE, FINAL);
+    type D_ARRAY is array (7 downto 0) of unsigned(8 downto 0);
+    signal distances, nextDistances : D_ARRAY;
+    signal XorY, nextXorY : integer := 2;
+    signal min_distance, nextMinDistance : unsigned(8 downto 0); --ho calcolato il quadrato
+    signal currState, nextState, prevState, nextPrevState : STATO := BEGINNER;
+    signal bitMask,nextBitMask: std_logic_vector(7 downto 0);
+    signal Xp,nextXp,Yp,nextYp,Xc,nextXc,Yc,nextYc : signed(8 downto 0);
+    signal maskPtr,nextMaskPtr : integer := 7;
+    signal cntrPtr,nextCntrPtr : integer := 1;
+    signal distance, nextDistance : unsigned(8 downto 0);
+    signal outMask, nextOutMask : std_logic_vector(7 downto 0);                
 begin
-    delta_lambda : process( PS, PRS, i_start, i_data, bitMask, Yo, Xo, Yp, Xp, Ydiff, Xdiff, dist_tmp,
-    min_distance, distances, Y_out, counter, counter2,
-    counter_prev, counter2_prev, min_distance_prev, Xp_prev, Xo_prev, Yo_prev, bitMask_prev, PRS_prev )
-        begin
-        NS <= PS;
-        PRS <= PRS_prev;
-        counter <= counter_prev;
-        bitMask <= bitMask_prev;
-        counter2 <= counter2_prev;
-        Xo <= Xo_prev;
-        Yo <= Yo_prev;
-        Xp <= Xp_prev;
-        min_distance <= min_distance_prev;
-        Yp <= (others => '0');
-        Xdiff <= (others => '0');
-        Ydiff <= (others => '0');
-        dist_tmp <= (others => '0');
-        o_data <= (others => '0');
-        Y_out <= (others => '0');
+    delta_lambda : process(currState, nextState, i_start, i_clk, XorY,cntrPtr,i_data,bitMask,maskPtr,Xc,Xp,Yc,Yp,distance,
+    min_distance,distances, outMask,prevState,nextMinDistance, nextOutMask,nextCntrPtr,nextMaskPtr,nextXp,nextYp,
+    nextXc, nextYc, nextDistances, nextPrevState,nextBitMask,nextDistance,nextXorY)
+    
+        begin   
+        nextState <= currState;
+        
+        XorY <= nextXorY;
+        min_distance <= nextMinDistance;
+        outMask <= nextOutMask;
+        cntrPtr <= nextCntrPtr;
+        maskPtr <= nextMaskPtr;
+        Xp <= nextXp;
+        Yp <= nextYp;
+        Xc <= nextXc;
+        Yc <= nextYc;
+        distances <= nextDistances;
+        prevState <= nextPrevState;
+        bitMask <= nextBitMask;
+        distance <= nextDistance;
+        
+        o_address <= std_logic_vector(to_unsigned(cntrPtr,16));   
+        o_data <= "00000000";
         o_done <= '0';
-        o_en <= '1';
+        o_en <= '0';
         o_we <= '0';
-        case PS is
-            when RST =>
-                o_address <= (others => '0');
-                o_data <= (others => '0');
-                NS <= S0;
-                PRS <= S0;
-                counter <= 1;
-                counter2 <= 7;
-            when S0 =>
-                if(i_start = '1') then
-                    o_address <= (4 => '1', 0 => '1', others => '0');
-                    NS <= S8;
-                    PRS <= S1;
-                else
-                    o_address <= (others => '0');
-                    PRS <= S0;
-                end if;
-            when S1 =>
-                o_address <= (4 => '1', 1 => '1', others => '0');
-                Xo <= unsigned("00" & i_data);
-                NS <= S8;
-                PRS <= S2;
-            when S2 =>
-                o_address <= (others => '0');
-                Yo <= unsigned("00" & i_data);
-                NS <= S8;
-                PRS <= S3;
-            when S3 =>
-                o_address <= std_logic_vector(to_unsigned(counter,16));
-                bitMask <= i_data;
-                NS <= S8;
-                PRS <= S4;
-            when S4 =>
-                counter <= counter + 1;
-                o_address <= std_logic_vector(to_unsigned(counter,16));
-                Xp <= unsigned("00" & i_data);
-                NS <= S8;
-                PRS <= S5;
-            when S5 =>
-                counter <= counter + 1;
-                counter2 <= counter2-1;
-                o_address <= std_logic_vector(to_unsigned(counter,16));
-                Yp <= unsigned("00" & i_data);
-                dist_tmp <= to_unsigned(520,10);
-                if bitMask(counter2) = '1' then
-                    if Yo > Yp then
-                        Ydiff <= Yo - Yp;
+        
+        nextXorY <= XorY;
+        nextMaskPtr <= maskPtr;
+        nextCntrPtr <= cntrPtr;
+        nextMinDistance <= min_distance;
+        nextXp <= Xp;
+        nextYp <= Yp;
+        nextXc <= Xc;
+        nextYc <= Yc;
+        nextOutMask <= outMask;
+        nextDistances <= distances;
+        nextBitMask <= bitMask;
+        nextDistance <= distance;
+        
+            case currState is                                
+                when BEGINNER =>
+                    nextPrevState <= BEGINNER;
+                    if i_start = '1' then                    
+                        nextState <= RST;
                     else 
-                        Ydiff <= Yp - Yo;
+                        nextState <= BEGINNER;
                     end if;
-                    if Xo > Xp then
-                        Xdiff <= Xo - Xp;
-                    else 
-                        Xdiff <= Xp - Xo;
-                    end if;
-                    dist_tmp <= Xdiff + Ydiff;
-                    if dist_tmp < min_distance then
-                        min_distance <= dist_tmp;
-                    end if;
-                end if; 
-                distances(8-counter/2) <= dist_tmp;
-                if counter = 17 then
-                    NS <= S6;
-                    PRS <= S6;
-                else
-                    NS <= S8;
-                    PRS <= S4;
-                end if;
-            when S6 =>
-                o_we <= '1';
-                counter <= 19;
-                o_address <= (4 => '1', 1 => '1', 0 => '1', others => '0');
-                for i in 0 to 7 loop
-                    dist_tmp <= distances(i);
-                    if dist_tmp = min_distance then
-                       Y_out(i) <= '1';
+                    
+                when RST =>
+                    nextXorY <= 2;
+                    nextMaskPtr <= 7;
+                    nextCntrPtr <= 1;
+                    nextDistances <= (others => to_unsigned(510,9));
+					nextMinDistance <= to_unsigned(510,9);
+					nextXp <= (others => '0');
+					nextYp <= (others => '0');
+					nextXc <= (others => '0');
+					nextYc <= (others => '0');
+					nextOutMask <= (others => '0');
+                    o_address <= std_logic_vector(to_unsigned(cntrPtr,16)); 
+                    nextState <= READ;
+                    nextPrevState <= RST;  
+                    
+                when RAM_WAIT =>
+                    o_en <= '1';
+                    nextPrevState <= prevState;
+                    if (prevState = RST or prevState = BEGIN_COMPUTE) then
+                        nextState <= SAVE;
+                        o_address <= std_logic_vector(to_unsigned(cntrPtr,16)); 
                     else
-                       Y_out(i) <= '0';
+                        nextState <= FINAL;
+                    end if;
+                    
+                when READ =>
+                    o_en <= '1';          
+                    if prevState = RST then 
+                        if XorY = 0 then
+                            nextCntrPtr <= 17;
+                        elsif XorY = 1 then
+                            nextCntrPtr <= 18;
+                        elsif XorY = 2 then
+                            nextCntrPtr <= 0; 
+                        end if;
+                    elsif prevState = BEGIN_COMPUTE then
+                        nextCntrPtr <= cntrPtr; 
+                    end if;
+                    o_address <= std_logic_vector(to_unsigned(cntrPtr,16));
+                    nextState <= RAM_WAIT;
+                    nextPrevState <= prevState;
+                
+                when SAVE =>                   
+                    if prevState = RST then 
+                        if XorY = 0 then
+                            nextXp <= signed('0' & i_data);
+                            nextXorY <= 1;
+                            nextState <= READ;
+                            nextPrevState <= prevState; 
+                        elsif XorY = 1 then
+                            nextYp <= signed('0' & i_data);
+                            nextXorY <= 0;
+                            nextCntrPtr <= 1;
+                            nextMaskPtr <= 7;
+                            nextState <= BEGIN_COMPUTE;
+                            nextPrevState <= BEGIN_COMPUTE;
+                        else
+                            nextBitMask <= i_data;
+                            nextXorY <= 0;
+                            nextState <= READ;
+                            nextPrevState <= prevState; 
+                        end if;              
+                    elsif prevState = BEGIN_COMPUTE then
+                        if XorY = 0 then
+                            nextXc <= signed('0' & i_data);
+                            nextXorY <= 1;
+                            nextState <= READ;                          
+                        else 
+                            nextYc <= signed('0' & i_data);
+                            nextXorY <= 0;
+                            nextState <= COMPUTE;
+                        end if;                
+                        nextCntrPtr <= cntrPtr + 1;
+                        nextPrevState <= prevState; 
+                    end if;
+                
+                when BEGIN_COMPUTE => 
+                    nextPrevState <= BEGIN_COMPUTE;
+                    if cntrPtr = 17 then
+                        nextState <= COMPARE;
+                    else
+                        if bitMask(maskPtr) = '1' then
+                            nextXorY <= 0;
+                            nextState <= READ;
+                        else 
+                            nextCntrPtr <= cntrPtr + 2;
+                            nextDistances(maskPtr) <= to_unsigned(510,9);
+                            nextMaskPtr <= maskPtr - 1;
+                            nextState <= BEGIN_COMPUTE;
+                        end if;
+                    end if;
+                                      
+                when COMPUTE => 
+                    nextPrevState <= prevState;                
+                    nextDistance <= unsigned(abs(Xc - Xp)+abs(Yc - Yp));
+                    if nextDistance < min_distance then
+                        nextMinDistance <= nextDistance;              
                     end if; 
-                end loop;
-                o_data <= Y_out;
-                NS <= S8;
-                PRS <= S7;
-                -- Faccio i calcoli di uguaglianza, costruisco il mio vettore di uscita, passo alla scrittura in uscita e vado in S9, da S9 poi passerò a S7
-            when S7 =>
-                o_done <= '1'; 
-                o_en <= '0';
-                o_address <= (others => '0');
-                PRS <= S7;
-                if i_start = '0' then
-                    o_done <= '0';
-                    NS <= RST;
-                else                     
-                    NS <= S7;
-                end if;
-                -- Setto un done a 1, e aspetto che start torni a 0 per poter tornare a S0 nel prossimo ciclo di clock, altrimenti rimango in questo stato.               
-            when S8 =>
-                o_address <= std_logic_vector(to_unsigned(counter,16));
-                NS <= PRS;
-        end case;
+                    nextDistances(maskPtr) <= nextDistance;        
+                    nextMaskPtr <= maskPtr - 1;
+                    
+                    if cntrPtr < 17 then
+                        nextState <= BEGIN_COMPUTE;
+                    else
+                        nextState <= COMPARE;
+                    end if;
+                
+                when COMPARE =>
+                    nextPrevState <= prevState; 
+                    for i in 0 to 7 loop
+                        if distances(i) = min_distance then
+                            outMask(i) <= '1';
+                        else
+                            outMask(i) <= '0';
+                        end if;
+                    end loop;
+                    nextState <= WRITE;
+                
+                when WRITE =>
+                    o_en <= '1';
+                    o_we <= '1';
+                    o_data <= outMask;
+                    nextCntrPtr <= 19;
+                    o_address <= std_logic_vector(to_unsigned(nextCntrPtr,16));
+                    nextPrevState <= currState;
+                    nextState <= RAM_WAIT;
+                                    
+                when FINAL =>
+                    o_done <= '1';
+                    if (i_start = '0') then
+                        o_done <= '0';
+                        nextState <= BEGINNER;
+                    else
+                        nextState <= currState;
+                    end if;
+                    nextPrevState <= prevState; 
+            end case;                
     end process;
     state: process( i_clk )
         begin
-        if i_clk'event and i_clk = '1'  then
-            if i_rst = '1' then
-                PS <= RST;
+        if( i_clk'event and i_clk = '1' ) then
+            if( i_rst = '1' ) then
+                currState <= BEGINNER;
             else
-                PS <= NS;
-                counter_prev <= counter;
-                counter2_prev <= counter2;
-                Xp_prev <= Xp;
-                Xo_prev <= Xo;
-                Yo_prev <= Yo;
-                bitMask_prev <= bitMask;
-                PRS_prev <= PRS;
-                min_distance_prev <= min_distance;
+                currState <= nextState;                                  
             end if;
         end if;
-    end process;
+    end process; 
 end Behavioral;
